b713cff7352aebee67d6c1f75a2666d3ef0764ca
jaunitageorge1 <jaunitageorge@gmail.com>
2023-02-27 10:12:56 -0500

    Exercise 1

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is a calculator. There's a catch though -- it only does division.");
+            int a = 17; 
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b; 
+
+            if (a == 17 && b == 4)
+            {
+                Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            }
+
         }
     }
 }
